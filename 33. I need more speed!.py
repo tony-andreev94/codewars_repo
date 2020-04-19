b3cc26e67f33602f4685121093325749e66267aa
Tony <tony.andreev94@outlook.com>
2020-04-19 14:53:22 +0300

    # unfinished.

--- 33. I need more speed!.py
+++ 33. I need more speed!.py
@@ -19,14 +19,57 @@ All other usual methods of the list class are still present.
 """
 
 
+# not accepted by codewars
 def reverse(seq):
     result = list()
     for i in range(len(seq) - 1, -1, -1):
         result.append(seq[i])
-    return result
+    seq = result
+    return seq
 
 
+# performance ineffective - time out
+def reverse2(seq):
+    up_bound = len(seq)
+    index = len(seq) - 1
+    while index >= 0:
+        seq.append(seq[index])
+        index -= 1
+
+    for i in range(up_bound):
+        seq.pop(0)
+    return seq
+
+
+# performance ineffective, exec time x2.3
+def reverse3(seq):
+    for i in range(-1, -(len(seq) + 1), -1):
+        seq.append(seq[i])
+        if i == -len(seq):
+            seq.pop(0)
+        else:
+            seq.pop(i - 1)
+    return seq
+
+
+def reverse4(seq):
+    length = len(seq)
+    counter = -1
+    while True:
+        if abs(counter) == length:
+            seq.append(seq[counter])
+            seq.pop(0)
+            break
+
+        seq.append(seq[counter])
+        counter -= 1
+        seq.pop(counter)
+    return seq
+
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+#            30, 31, 32, 33, 34, 35, 36, 37, 38, 10, 39, 40]
 my_list = [1, 2, 3, 4, 5]
 print(reverse(my_list))
 my_list2 = [{'b': 2}, {'a': 1}]
-print(reverse(my_list2))
+print(reverse4(my_list2))
